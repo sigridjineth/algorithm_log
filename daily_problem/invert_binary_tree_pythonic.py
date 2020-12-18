e78481ce6508e0efb683728a33d0d44bf04542d1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-12-18 20:52:55 +0900

    2020.12.18 Invert Binary Tree LEETCODE #226 Recursive Way - self.invertTree의 파라미터로 root.right을 넘겼으므로 오른쪽부터 탐색한다. 오른쪽이 완료되면 왼쪽을 탐색한다. 다 이루어지면 위로 올라가서 오른쪽과 왼쪽을 서로 스왑한다.

---
+++ daily_problem/invert_binary_tree_pythonic.py
@@ -0,0 +1,15 @@
+# 이진 트리 반전 LEETCODE #206
+# 파이썬답게 재귀를 활용한 방식
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
