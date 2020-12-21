6d903f26cc9e6980e89ec62636c011de264bee17
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-12-21 19:37:41 +0900

    2020.12.21 이진 탐색 트리(BST)를 더 큰 수 합계 트리로 LEETCODE #1038 문제풀이 - 자신보다 같거나 큰 값을 구하려면 자기 자신을 포함한 우측 자식 노드의 합을 구하면 된다. - BST의 우측 자식 노드는 항상 부모 노드보다 큰 값이기 때문이다. - root를 입력받았을 때 먼저 맨 오른쪽까지 내려가고, 그 다음 부모 노드, 다시 왼쪽 노드 순으로 이동하면서 자신의 값을 포함해 누적한다. - 중위 순회로 움직이며, 오른쪽-부모-왼쪽 순으로 이어진다. - self.val은 지금까지 누적된 값이며, root.val은 현재 노드의 값이다. - 중위 순회를 하면서 현재 노드의 값을 자기 자신을 포함한 지금까지의 누적된 값과 합치는 논리이다.

---
+++ daily_problem/binary_search_tree_to_greater_sum_tree.py
@@ -0,0 +1,17 @@
+# Binary Search Tree to Greater Sum Tree
+# LEETCODE 1038번
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    val: int = 0
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 중위 순회 노드 값 누적
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+        return root
