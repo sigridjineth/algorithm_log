c73f77d4097e546df0eba6f481a37da6a04f2629
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-12-23 21:00:41 +0900

    2020.12.23 Implement Trie LEETCODE #208 - 파이썬의 딕셔너리, 자바스크립트의 오브젝트를 이용하여 간결하게 구현할 수 있다.

---
+++ daily_problem/implement_trie.py
@@ -0,0 +1,30 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+    
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+    
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
