706ce303113c3964935028b8df75c0823bb410a3
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-02-08 19:53:55 +0900

    2021.02.08 카카오 2019 실패율 - 정렬 문제인데 기본적으로 구현을 잘 하면 된다.

---
+++ Programmers/KAKAO/실패율.py
@@ -0,0 +1,21 @@
+# 카카오 2019 실패율
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+
+    # 스테이지 번호를 1부터 N까지 증가시키며
+    for i in range(1, N+1):
+        # 해당 스테이지 머물러 있는 사람의 수 계산
+        count = stages.count(i)
+        if (length == 0):
+            fail = 0
+        else:
+            fail = (count / length)
+        # 리스트에 (스테이지 번호, 실패율) 원소 삽입
+        answer.append((i, fail))
+        length -= count
+    # 실패율을 기준으로 각 스테이지를 내림차순 정렬
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+    # 정렬된 스테이지 번호 반환
+    answer = [i[0] for i in answer]
+    return answer
