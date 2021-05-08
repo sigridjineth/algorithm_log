ddd4a30e526ae016d2d5f2f39dcc7447fe5e956b
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-05-08 13:39:28 +0900

    feat: 외판원순회 2 10971번 - 중복없이 세려면 순열을 이용하여 순서를 만들 수 있다. 중복 없이 N개의 모든 도시를 거치고 다시 원래 도시로 복귀해야 하기 때문이다. - sum += a[d[i]][d[i+1]] - a[d[i]][d[i+1]]이 0인 경우 존재하지 않았거나, 마지막 도시에서 처음 도시로 복귀하는 경우 내용이 0이 아닌 경우 최솟값을 갱신한다.

---
+++ codeplus/src/외판원순회2_Q10971.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class 외판원순회2_Q10971 {
+	public static int answer = Integer.MAX_VALUE;
+
+	private static boolean next_permutation(int[] a) {
+		int i = a.length - 1;
+		while (i > 0 && a[i-1] >= a[i]) {
+			i -= 1;
+		}
+		if (i <= 0) {
+			return false;
+		}
+
+		int j = a.length - 1;
+		while (a[i-1] >= a[j]) {
+			j -= 1;
+		}
+
+		int temp = a[i-1];
+		a[i-1] = a[j];
+		a[j] = temp;
+
+		j = a.length - 1;
+		while (i < j) {
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i += 1;
+			j -= 1;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] a = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+
+		int[] d = new int[n];
+		for (int i = 0; i < n; i++) {
+			d[i] = i;
+		}
+
+		do {
+			boolean not_zero = true;
+			int sum = 0;
+			for (int i = 0; i < n - 1; i++) {
+				if (a[d[i]][d[i+1]] == 0) {
+					not_zero = false;
+				} else {
+					sum += a[d[i]][d[i+1]];
+				}
+			}
+			if (not_zero && a[d[n-1]][d[0]] != 0) {
+				sum += a[d[n-1]][d[0]];
+				if (answer > sum) {
+					answer = sum;
+				}
+			}
+		} while (next_permutation(d));
+		System.out.println(answer);
+	}
+}
