5fb239ba49402bf077830722aba4b378d9ef165f
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-05-19 00:23:18 +0900

    feat: Q11052 카드 구매하기 Top-down 방식 - 탑다운 방식으로 구현! https://ldgeao99.tistory.com/287

--- codeplus/src/Q11052_카드구매하기.java
+++ codeplus/src/Q11052_카드구매하기.java
@@ -1,5 +1,34 @@
 import java.util.Scanner;
 
+class Q11052_카드구매하기_2 {
+
+	static int[] d;
+	static int[] a;
+
+	static int getMaxPrice(int n) {
+		if (n == 0) {
+			return 0;
+		} else if (d[n] > 0) {
+			return d[n];
+		}
+		for (int i = 1; i <= n; i++) {
+			d[n] = Math.max(d[n], getMaxPrice(n - i)+ a[i]);
+		}
+		return d[n];
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		a = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = sc.nextInt();
+		}
+		d = new int[n + 1];
+		System.out.println(getMaxPrice(n));
+	}
+}
+
 public class Q11052_카드구매하기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
