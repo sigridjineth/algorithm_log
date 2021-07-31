53a0e3ebbe5f1734323933e4d3b1cc200b98ea87
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-07-31 18:27:33 +0900

    feat: 벽 부수고 이동하기 2 Q14442
    - 벽을 k개로 부술 경우, k - 1개로 부술 경우, ... 하나도 부시지 않을 경우..가 있다.
    - 따라서 z + 1이 k를 넘어가는 지만 체크하면 된다.

---
+++ codeplus/src/벽부수고_이동하기2_Q14442.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Pair {
+    int x, y, z;
+    Pair(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+public class Main {
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int l = sc.nextInt();
+        sc.nextLine();
+        int[][] a = new int[n][m];
+        int[][][] d = new int[n][m][l+1];
+        for (int i=0; i<n; i++) {
+            String s = sc.nextLine();
+            for (int j=0; j<m; j++) {
+                a[i][j] = s.charAt(j) - '0';
+            }
+        }
+        d[0][0][0] = 1;
+        Queue<Pair> q = new LinkedList<Pair>();
+        q.offer(new Pair(0, 0, 0));
+        while (!q.isEmpty()) {
+            Pair p = q.remove();
+            int x = p.x;
+            int y = p.y;
+            int z = p.z;
+            for (int k=0; k<4; k++) {
+                int nx = x+dx[k];
+                int ny = y+dy[k];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (a[nx][ny] == 0 && d[nx][ny][z] == 0) {
+                    d[nx][ny][z] = d[x][y][z] + 1;
+                    q.offer(new Pair(nx, ny, z));
+                }
+                if (z+1 <= l && a[nx][ny] == 1 && d[nx][ny][z+1] == 0) {
+                    d[nx][ny][z+1] = d[x][y][z] + 1;
+                    q.offer(new Pair(nx, ny, z+1));
+                }
+            }
+        }
+        int ans = -1;
+        for (int i=0; i<=l; i++) {
+            if (d[n-1][m-1][i] == 0) continue;
+            if (ans == -1) {
+                ans = d[n-1][m-1][i];
+            } else if (ans > d[n-1][m-1][i]) {
+                ans = d[n-1][m-1][i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
