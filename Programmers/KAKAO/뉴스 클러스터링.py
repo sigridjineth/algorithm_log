22fc38af40c8a4019b3e1b6842de9f616a35be10
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-09-11 00:36:16 +0900

    Update 뉴스 클러스터링.py

--- Programmers/KAKAO/뉴스 클러스터링.py
+++ Programmers/KAKAO/뉴스 클러스터링.py
@@ -1,34 +1,24 @@
-import collections, re
+from collections import Counter
+import re
 
 def solution(str1, str2):
-    # 두 글자씩 끊어보자
-    new_str1 = [
-        # 리스트 컴프리헨션 처리
-        str1[i:i+2].lower()
-        for i in range(len(str1)-1)
-        if re.findall('[a-z]{2}', str1[i:i+2].lower()) # regex 처리
+    str1s = [
+        str1[i:i + 2].lower()
+        for i in range(len(str1) - 1)
+        if (re.findall('[a-z]{2}', str1[i:i + 2].lower()))
     ]
-    new_str2 = [
-        # 리스트 컴프리헨션 처리
-        str2[i:i+2].lower()
-        for i in range(len(str2)-1)
-        if re.findall('[a-z]{2}', str2[i:i+2].lower()) # regex 처리
+    str2s = [
+        str2[i:i + 2].lower()
+        for i in range(len(str2) - 1)
+        if (re.findall('[a-z]{2}', str2[i:i + 2].lower()))
     ]
-    # 교집합 계산
-    # collections.Counter(new_str1) 등으로 각자 리스트의 counter를 계산한다.
-    # 이후 AND 연산을 해서 나오는 dictionary 형태를 values로 따온다.
-    # 마지막으로 이를 sum하여 교집합의 결과를 계산한다.
-    intersection = sum((collections.Counter(new_str1) & collections.Counter(new_str2)).values())
-
-    # 합집합 계산
-    union = sum((collections.Counter(new_str1) | collections.Counter(new_str2)).values())
-
-    jac = 1
-
+    
+    intersections = sum((Counter(str1s) & Counter(str2s)).values())
+    union = sum((Counter(str1s) | Counter(str2s)).values())
+    
     if (union == 0):
-        return int(jac * 65536)
+        jaccard_sum = 1
     else:
-        jac = intersection / union
-        return int(jac * 65536)
-
-print(solution('FRANCE', 'french'))
+        jaccard_sum = (intersections / union)
+    
+    return int(jaccard_sum * 65536)
