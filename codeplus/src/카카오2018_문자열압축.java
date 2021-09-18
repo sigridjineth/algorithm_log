e7eea056f855596683d484cc9c1645f70de4afe4
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-09-18 19:47:53 +0900

    feat: 카카오 2020 문자열 압축

--- codeplus/src/카카오2018_문자열압축.java
+++ codeplus/src/카카오2018_문자열압축.java
@@ -1,19 +1,24 @@
 public class 카카오2018_문자열압축 {
 	public static void main(String[] args) {
-		String s = "ababcdcdababcdcd";
+		String s = "abcabcdede";
 		System.out.println(solution(s));
 	}
-	public static int solution(String s) {
+	public static String solution(String s) {
 		String answer = s;
 		int MAX_CUT_LENGTH = s.length() / 2;
 		int STR_LENGTH = s.length();
-		for (int i = 1; i < MAX_CUT_LENGTH; i++) {
-			int count = 0;
+		for (int i = 1; i < MAX_CUT_LENGTH + 1; i++) {
+			int count = 1;
 			StringBuilder temp_str = new StringBuilder();
 			String bench_str = s.substring(0, i);
 			for (int j = i; j < STR_LENGTH; j += i) {
-				String str = s.substring(j, j + i);
-				if (bench_str.equals(str)) {
+				StringBuilder sub = new StringBuilder();
+				for (int k = j; k < j + i; k++) {
+					if (k < STR_LENGTH) {
+						sub.append(s.charAt(k));
+					}
+				}
+				if (bench_str.equals(sub.toString())) {
 					count += 1;
 				} else {
 					if (count > 1) {
@@ -21,14 +26,19 @@ public class 카카오2018_문자열압축 {
 					} else {
 						temp_str.append(bench_str);
 					}
-					bench_str = str;
+					bench_str = sub.toString();
 					count = 1;
 				}
+			}
+			if (count > 1) {
+				temp_str.append(count).append(bench_str);
+			} else {
+				temp_str.append(bench_str);
 			}
 			if (answer.length() > temp_str.length()) {
 				answer = temp_str.toString();
 			}
 		}
-		return answer.length();
+		return answer;
 	}
 }
